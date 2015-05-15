4b5ec98b3a3208f4415844524353a6f1cc2eee5f
Jonathan Schwarz <SchwarzJn@googlemail.com>
2015-05-15 19:40:24 +0200

    implemented function unrolling

--- logic/src/srn.cpp
+++ logic/src/srn.cpp
@@ -5,6 +5,8 @@
 //Filename           : srn.cpp
 //------------------------------------------------------
 
+#include <random.h>
+
 #include "srn.hpp"
 #include "srnNeuron.hpp"
 
@@ -14,8 +16,12 @@ srn::srn(const std::vector<unsigned> &topology, const bool &bias)
     unsigned numLayers = topology.size();
 
     m_recentAverageSmoothingFactor = 100.0; // Number of training samples to average over
+    m_initialX                     = 0.0; 
+    m_iterations                   = 0;
 
-    for (unsigned nbLayer = 0; nbLayer < numLayers; ++nbLayer) {
+
+    for (unsigned nbLayer = 0; nbLayer < numLayers; ++nbLayer) 
+    {
         m_layers.push_back(Layer());
         unsigned numOutputs;
         unsigned neuronNum = 0;
@@ -29,8 +35,53 @@ srn::srn(const std::vector<unsigned> &topology, const bool &bias)
 
         // We have a new layer, now fill it with neurons
         for (neuronNum = 0; neuronNum < topology[nbLayer]; ++neuronNum) 
+            m_layers.back().push_back(new srnNeuron(numOutputs, neuronNum, MOMENTUM, LEARNING_RATE));
+
+        // Create bias neurons if requested
+        if(false != bias)
         {
-                m_layers.back().push_back(new srnNeuron(numOutputs, neuronNum, MOMENTUM, LEARNING_RATE));
+            m_layers.back().push_back(new srnNeuron(numOutputs, ++neuronNum, MOMENTUM, LEARNING_RATE));
+            // Force the bias node's output to 1.0 (it was the last recent neuron pushed in this layer):
+            m_layers.back().back()->setOutputVal(1.0);
+        }
+    }
+}
+
+srn(const std::vector<unsigned> &topology, const size_t &timeHorizon, const bool &bias)
+{
+    m_bias = bias;
+    m_timeHorizon = m_timeHorizon;
+
+    unsigned numLayers = topology.size();
+
+    m_recentAverageSmoothingFactor = 100.0; // Number of training samples to average over
+    m_initialX   = 0.0; 
+    m_iterations = 0;
+
+    for (unsigned nbLayer = 0; nbLayer < numLayers; ++nbLayer) 
+    {
+        m_layers.push_back(Layer());
+        unsigned numOutputs;
+        unsigned neuronNum = 0;
+
+        if(topology.size() - 1 == nbLayer)
+           // Output neurons don't have outputs
+           numOutputs = 0;
+        else
+           // The number of outputs is equal to the number of neurons in the next layer
+           numOutputs = topology[nbLayer + 1];
+
+
+        // We have a new layer, now fill it with neurons
+        for (neuronNum = 0; neuronNum < topology[nbLayer]; ++neuronNum) 
+        {
+            m_layers.back().push_back(new srnNeuron(numOutputs, neuronNum, MOMENTUM, LEARNING_RATE));
+            if((topology.size() - 1 != nbLayer) && (0 != nbLayer))
+            {
+                // Hidden layer, create input queues for function unrolling
+                m_recurrentWeights.push_back(rand() / double(RAND_MAX));
+                m_recurrentDeltaWeights.push_back(0.0);
+            }
         }
 
         // Create bias neurons if requested
@@ -57,16 +108,31 @@ void srn::feedForward(const std::vector<double> &inputVals)
     for (unsigned i = 0; i < inputVals.size(); ++i) 
         m_layers[0][i]->setOutputVal(inputVals[i]);
 
+    // Save current input for the hidden layer
+    for (unsigned nbLayer = 1; nbLayer < m_layers.size()-1; ++nbLayer) 
+    {
+        unsigned nbNeuronsInLayer = m_layers[nbLayer].size() - (1*m_bias);
+        double   currentNeuronInput = 0.0;
+        for (unsigned n = 0; n < nbNeuronsInLayer; ++n) 
+        {
+            currentNeuronInput = m_layers[nbLayer][n]->getInput(m_layers, nbLayer);
+            m_recentInputs[0].push(currentNeuronInput);
+        }
+    }
+
+    m_iterations++;
+    if (m_timeHorizon > m_iterations) 
+        unroll(m_iterations);
+    else
+        unroll(m_timeHorizon);
+
     // forward propagate
     for (unsigned nbLayer = 1; nbLayer < m_layers.size(); ++nbLayer) 
     {
         unsigned nbNeuronsInLayer = m_layers[nbLayer].size() - (1*m_bias);
-
         for (unsigned n = 0; n < nbNeuronsInLayer; ++n) 
             m_layers[nbLayer][n]->feedForward(m_layers, nbLayer);
     }
-
-    unroll();
 }
 
 void srn::learn(const std::vector<double> &targetVals)
@@ -124,33 +190,59 @@ void srn::learn(const std::vector<double> &targetVals)
     }
 }
 
-void srn::unroll()
+void srn::unroll(const size_t &times)
 {
-    std::vector<Layer>::iterator it = (m_layers.end())-1;
-    std::vector<Layer>::iterator itMinus = it - 1;
+    std::vector<Layer>::iterator it = (m_layers.end()) - 1;
 
-    m_layers.insert(it, Layer());
+    // Unfolding does not include the bias neuron
+    unsigned neuronsToAdd = it.size() - (1*m_bias);;
 
-    unsigned numOutputs = m_layers.end()->size();
-    unsigned neuronNum = 0;
-
-    for (; neuronNum < it->size(); ++neuronNum) 
-        it->push_back(new srnNeuron(numOutputs, neuronNum, MOMENTUM, LEARNING_RATE));
-
-    // Copy bias neuron for output layer to rightmost hidden layer
-    it->push_back(new srnNeuron(*dynamic_cast<srnNeuron*>(itMinus->back())));
-    delete itMinus->back();
-
-    // Save current input x_T of preceeding hidden layer by creating constant neurons in the same layer.
-    // Those neurons have only one link
-    neuronNum = itMinus->size()-1;
-    unsigned neuronsToAdd = itMinus->size();
-
-    for (unsigned n = 0; n < neuronsToAdd; ++n, ++neuronNum)
+    for (int i = 0; i < times; ++i)
     {
-        itMinus->push_back(new srnNeuron(1, neuronNum, MOMENTUM, LEARNING_RATE));
-        double x_T = dynamic_cast<srnNeuron*>((*itMinus)[n])->getInput();
-        itMinus->back()->setOutputVal(x_T);
-        dynamic_cast<srnNeuron*>(itMinus->back())->singleConnection(n, 1.0);
+        // Create a new layer for the respective time step, set iterator to newly created layer.
+        m_layers.insert(it--, Layer());
+        std::vector<Layer>::iterator itMinus = it - 1;
+
+        unsigned neuronNum = 0;
+
+        for (; neuronNum < neuronsToAdd; ++neuronNum) 
+        {
+            // Neurons created during unfolding only have one connection to themselves 
+            // in the next time step
+            it->push_back(new srnNeuron(0, neuronNum, MOMENTUM, LEARNING_RATE));
+
+            // Weight of the feedback connection
+            it->back().m_outputWeights[0].weight      = m_recurrentWeights[neuronNum];
+            it->back().m_outputWeights[0].deltaWeight = m_recurrentDeltaWeights[neuronNum];
+        }
+
+        // Input x(t) will be added by creating a constant neuron in the preceding layer.
+        // Those neurons have only one link
+        for (unsigned n = 0; n < neuronsToAdd; ++n, ++neuronNum)
+        {
+            it->push_back(new srnNeuron(1, neuronNum, MOMENTUM, LEARNING_RATE));
+
+            std::queue<double> newQueue;
+            
+            if ((0 != n) || (times != m_timeHorizon)) 
+            {
+                double x_T = m_recentInputs[0].pop();
+                newQueue.push(x_T);
+            }
+            else
+                double x_T = m_recentInputs[0].pop();
+
+            it->back()->setOutputVal(x_T);
+
+            // w value
+            dynamic_cast<srnNeuron*>(it->back())->singleConnection(n, 1.0, 0.0);
+
+            m_recentInputs[0] = newQueue;
+        }
+
     }
+}
+
+void srn::rollup(const size_t &times)
+{
 }
