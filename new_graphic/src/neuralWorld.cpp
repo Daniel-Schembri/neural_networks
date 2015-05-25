64f1a2035c84dfd7e61acfeca2db12df34b93931
Daniel Schembri <--global>
2015-05-25 15:45:59 +0200

    new graphic added license information in head of files

--- new_graphic/src/neuralWorld.cpp
+++ new_graphic/src/neuralWorld.cpp
@@ -1,3 +1,36 @@
+/*
+* Copyright (c) 2015 Daniel Schembri https://github.com/Daniel-Schembri/neural_networks
+*
+* neuralworld.cpp
+* This cpp-file uses Box2D by Erin Catto 
+*
+* new_graphic
+* This is a graphical user interface to evolve object collecting agents 
+* The agent have two neural networks for steering angle and velocity.
+*
+* This project includes the neural net framework, called logic,
+* of Jonathan Schwarz https://github.com/jonathan-schwarz/neural_networks
+*
+* new_graphic also uses the Box2D-library by Erin Catto
+* At following the license for Box2D:
+*
+* Copyright (c) 2006-2007 Erin Catto http://www.box2d.org
+*
+* This software is provided 'as-is', without any express or implied
+* warranty.  In no event will the authors be held liable for any damages
+* arising from the use of this software.
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely, subject to the following restrictions:
+* 1. The origin of this software must not be misrepresented; you must not
+* claim that you wrote the original software. If you use this software
+* in a product, an acknowledgment in the product documentation would be
+* appreciated but is not required.
+* 2. Altered source versions must be plainly marked as such, and must not be
+* misrepresented as being the original software.
+* 3. This notice may not be removed or altered from any source distribution.
+*/
+
 #include "neuralWorld.h"
 
 
