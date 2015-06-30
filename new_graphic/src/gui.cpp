57a27810cad825a432ed911d1d0c8dadb86fadd5
Daniel Schembri <--global>
2015-06-30 09:22:29 +0200

    GUI removed a graphic bug on zooming

--- new_graphic/src/gui.cpp
+++ new_graphic/src/gui.cpp
@@ -319,13 +319,6 @@ void gui::Resize(int w, int h)
 
 	// L/R/B/T
 	gluOrtho2D(lower.x, upper.x, lower.y, upper.y);
-
-    //Clear 1st GLUT-Window
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glMatrixMode(GL_MODELVIEW);
-	glutSwapBuffers();
-
 }
 
 void gui::SimulationLoop()
